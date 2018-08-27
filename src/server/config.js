350a84a3c5e5b8c93ef6868d7d087f6445f76f3f
Chris Lee <christopher.p.lee@vanderbilt.edu>
2018-08-27 14:04:04 -0700

    removed fromt git for security

--- src/server/config.js
+++
@@ -1,6 +0,0 @@
-// Copy this file as config.js in the same folder, with the proper database connection URI.
-
-module.exports = {
-  iamUser: 'AKIAIJFGYIJYGQJFJRVQ',
-  iamSecret: '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',
-};
